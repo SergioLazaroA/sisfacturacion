c29aa7e26be8635e80c9969c891f96341ec1632f
Sergio Lazaro A <sergiolazaroapaza2006@gmail.com>
2025-05-04 16:55:34 -0500

    Se creo la funcion agregar cliente

--- src/main/java/com/mycompany/sisfacturacion/Sisfacturacion.java
+++ src/main/java/com/mycompany/sisfacturacion/Sisfacturacion.java
@@ -13,4 +13,9 @@ public class Sisfacturacion {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    public void agregarCliente(String nombre)
+    {
+        System.out.println("Cliente agregado"+ nombre);
+    }
 }
