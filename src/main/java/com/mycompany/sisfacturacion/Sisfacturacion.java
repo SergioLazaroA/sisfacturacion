3e957984f613529266a3fa3e0dd7a662539a5f9f
Sergio Lazaro A <sergiolazaroapaza2006@gmail.com>
2025-05-04 19:05:55 -0500

    Se aplicaron mejores practicas en la clase Sisfacturacion

--- src/main/java/com/mycompany/sisfacturacion/Sisfacturacion.java
+++ src/main/java/com/mycompany/sisfacturacion/Sisfacturacion.java
@@ -13,17 +13,19 @@ public class Sisfacturacion {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    
+    // Método que permite agregar un cliente nuevo al sistema
     public void agregarCliente(String nombre)
     {
-        System.out.println("Cliente agregado"+ nombre);
+        // Imprime un mensaje indicando que el cliente fue agregado exitosamente
+        System.out.println("Cliente agregado: "+ nombre);
     }
-    
+    // Método para agregar un plato al sistema
+    // Recibe el nombre del plato y su precio como parámetros
     public void agregarPlato(String nombrePlato, double precio)
     {
-        System.out.println("Plato agregado: "+nombrePlato+" con precio "+precio);
+        // Muestra en consola el plato agregado junto con su precio
+        System.out.println("Plato agregado: "+nombrePlato+" Con precio: "+precio);
     
-    }
-    
+    }   
     
 }
